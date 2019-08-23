498735e96b797d7dba0791e4b72833b8629a9cd7
tjlee527 <tjlee527@gmail.com>
2019-08-23 13:06:30 -0700

    added get request for home page

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3003;
+const connection = require('../database/dbConnect.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  connection.NearbyHouse.find(({parentHouseId: 1}))
+    .then((houses) => {
+      res.send(houses);
+    })
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
