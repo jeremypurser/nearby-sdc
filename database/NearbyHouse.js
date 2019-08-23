345fbe18f264299dafbf731563824e361b0e55c0
tjlee527 <tjlee527@gmail.com>
2019-08-23 13:51:41 -0700

    imported db const to NearbyHouse

--- database/NearbyHouse.js
+++ database/NearbyHouse.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const db = require('./index.js');
 const Schema = mongoose.Schema;
 
 var nearbySchema = new Schema({
--- server/index.js
+++ server/index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const NearbyHouse = require('../database/NearbyHouse.js');
 
 const app = express();
 const port = 3003;
-const connection = require('../database/dbConnect.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/house', (req, res) => {
-  connection.NearbyHouse.find(({parentHouseId: 1}))
+app.get('/', (req, res) => {
+  NearbyHouse.find(({parentHouseId: 1}))
     .then((houses) => {
       res.send(houses);
+    })
+    .catch((error) => {
+      res.send(error)
     })
 });
 
 app.get('/house/:houseId', (req, res) => {
-  connection.NearbyHouse.find(({parentHouseId: req.params.houseId}))
+  NearbyHouse.find(({parentHouseId: req.params.houseId}))
     .then((houses) => {
       res.send(houses);
+    })
+    .catch((error) => {
+      res.send(error)
     })
 });
 
