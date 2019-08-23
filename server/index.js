6b939f126eec4d7988de55de507620d88c96b759
tjlee527 <tjlee527@gmail.com>
2019-08-23 13:11:25 -0700

    added get request for specific houseid num

--- server/index.js
+++ server/index.js
@@ -7,8 +7,16 @@ const connection = require('../database/dbConnect.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/house', (req, res) => {
   connection.NearbyHouse.find(({parentHouseId: 1}))
+    .then((houses) => {
+      res.send(houses);
+    })
+});
+
+app.get('/house/:houseId', (req, res) => {
+  console.log(req.params);
+  connection.NearbyHouse.find(({parentHouseId: req.params.houseId}))
     .then((houses) => {
       res.send(houses);
     })
