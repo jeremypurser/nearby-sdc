70683c26f6bcb913c9ca8f2cce470202de498dfe
tjlee527 <tjlee527@gmail.com>
2019-08-23 17:01:37 -0700

    added get request when widget mounts using axios, changes state of houses based on data sent back

--- client/components/Widget.jsx
+++ client/components/Widget.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import Carousel from './Carousel.jsx';
+import axios from 'axios';
 
 class Widget extends React.Component {
   constructor() {
     super()
+    this.state = {
+      currentHouse: 1
+    }
+  }
+
+  componentDidMount() {
+    this.getNearbyHouses(this.state.currentHouse);
+  }
+
+  changeHouses(houses) {
+    this.setState({
+      houses: houses
+    })
+  }
+
+  getNearbyHouses(id) {
+    axios.get(`/house/${id}`)
+      .then((houses) => {
+        this.changeHouses(houses.data);
+      })
+      .catch((error) => {
+        console.log(`error getting houses ${error}`);
+      })
   }
 
   render() {
