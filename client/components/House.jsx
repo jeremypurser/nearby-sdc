51f8ad83070d78333fabcb286c41d96aa648a881
tjlee527 <tjlee527@gmail.com>
2019-08-28 16:48:09 -0700

    modified stars component to use styled-components

--- client/components/House.jsx
+++ client/components/House.jsx
@@ -26,13 +26,13 @@ class House extends React.Component {
           <img style={imgStyle} src={this.props.house.imgUrl}></img>
         </div>
         <div className='layout'>
-          <h5>{this.props.house.type}: {this.props.house.location}</h5>
+          <h5>{this.props.house.type}: {this.props.house.location} </h5>
         </div>
         <div className='description'>
-          <h3>{this.props.house.title}</h3>
+          <h3>{this.props.house.title} </h3>
         </div>
         <div className='cost'>
-          <h5>{this.props.house.cost}</h5>
+          <h5>{this.props.house.cost} </h5>
         </div>
         <div className='stars'>
           <h5><Stars key={this.props.house._id} rating={this.props.house.stars} /> {this.props.house.reviewCount}</h5>
