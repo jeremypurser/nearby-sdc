5f8da39afd5338e6d4c33e91c5ce3c958b349d38
tjlee527 <tjlee527@gmail.com>
2019-08-28 15:10:33 -0700

    added styled components and modified button component for styled-component

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
+import styled from 'styled-components'; 
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 
-const buttonDiv = {
-  float: 'left',
-  width: '100px',
-  height: '500px',
-  display: 'flex',
-  textAlign: 'center',
-  justifyContent: 'center',
-  backgroundColor: 'transparent',
-  outlineStyle: 'solid'
-};
-const buttonStyle = {
-  backgroundColor: 'Transparent',
-  border: 'none',
-  overflow: 'hidden',
-  outline: 'none',
-  float: 'left'
-};
+// const buttonDiv = {
+//   float: 'left',
+//   width: '100px',
+//   height: '500px',
+//   display: 'flex',
+//   textAlign: 'center',
+//   justifyContent: 'center',
+//   backgroundColor: 'transparent',
+//   outlineStyle: 'solid'
+// };
+
+const ButtonStyle = styled.div`
+  background-color: transparent;
+  border: none;
+  overflow: hidden;
+  outline: none;
+  float: left;
+`
+// const buttonStyle = {
+//   backgroundColor: 'Transparent',
+//   border: 'none',
+//   overflow: 'hidden',
+//   outline: 'none',
+//   float: 'left'
+// };
 
 const Button = (props) => {
   return (
-    <button className='button' style={buttonStyle} onClick={props.buttonClickHandler} value={props.value}> {props.value === 'right' ?<FontAwesomeIcon id='right' icon={faChevronLeft} size='2x'/> : <FontAwesomeIcon icon={faChevronRight} size='2x'/>}</button>
+    <ButtonStyle>
+      <button className='button' onClick={props.buttonClickHandler} value={props.value}> {props.value === 'right' ?<FontAwesomeIcon id='right' icon={faChevronLeft} size='2x'/> : <FontAwesomeIcon icon={faChevronRight} size='2x'/>}</button>
+    </ButtonStyle>
   )
 }
 
