70fdc2de04d4f0adccdaf9234ac2de6a5985c2d7
tjlee527 <tjlee527@gmail.com>
2019-08-28 15:28:37 -0700

    added ButtonStyle styled-component to Button Component

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -5,38 +5,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
-
-// const buttonDiv = {
-//   float: 'left',
-//   width: '100px',
-//   height: '500px',
-//   display: 'flex',
-//   textAlign: 'center',
-//   justifyContent: 'center',
-//   backgroundColor: 'transparent',
-//   outlineStyle: 'solid'
-// };
-
-const ButtonStyle = styled.div`
+const ButtonStyle = styled.button`
   background-color: transparent;
   border: none;
   overflow: hidden;
   outline: none;
   float: left;
 `
-// const buttonStyle = {
-//   backgroundColor: 'Transparent',
-//   border: 'none',
-//   overflow: 'hidden',
-//   outline: 'none',
-//   float: 'left'
-// };
 
 const Button = (props) => {
   return (
-    <ButtonStyle>
-      <button className='button' onClick={props.buttonClickHandler} value={props.value}> {props.value === 'right' ?<FontAwesomeIcon id='right' icon={faChevronLeft} size='2x'/> : <FontAwesomeIcon icon={faChevronRight} size='2x'/>}</button>
-    </ButtonStyle>
+      <ButtonStyle className='button' onClick={props.buttonClickHandler} value={props.value}> {props.value === 'right' ?<FontAwesomeIcon id='right' icon={faChevronLeft} size='2x'/> : <FontAwesomeIcon icon={faChevronRight} size='2x'/>}</ButtonStyle>
   )
 }
 
