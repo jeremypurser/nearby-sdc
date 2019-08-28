d14dacb8ca9e150053e057c66dfa4e45b99655cc
tjlee527 <tjlee527@gmail.com>
2019-08-28 16:55:06 -0700

    modified House component to styled-component

--- client/components/House.jsx
+++ client/components/House.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import Stars from './Stars.jsx';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  width: 250px;
+  height: 200px;
+`
+const HouseContainer = styled.div`
+  float: left;
+  margin: 20px 20px 20px 20px;
+`
 
 class House extends React.Component {
   constructor(props){
@@ -11,19 +21,19 @@ class House extends React.Component {
   }
 
   render() {
-    const imgStyle = {
-      width: '250px',
-      height: '200px'
-    }
-    const divStyle = {
-      float: 'left',
-      margin: '20px 20px 20px 20px'
-    }
+    // const imgStyle = {
+    //   width: '250px',
+    //   height: '200px'
+    // }
+    // const divStyle = {
+    //   float: 'left',
+    //   margin: '20px 20px 20px 20px'
+    // }
 
     return (
-      <div className='house' style={divStyle} onClick={this.handleClick.bind(this)}>
+      <HouseContainer className='house' onClick={this.handleClick.bind(this)}>
         <div className='image'>
-          <img style={imgStyle} src={this.props.house.imgUrl}></img>
+          <Image src={this.props.house.imgUrl}></Image>
         </div>
         <div className='layout'>
           <h5>{this.props.house.type}: {this.props.house.location} </h5>
@@ -35,10 +45,9 @@ class House extends React.Component {
           <h5>{this.props.house.cost} </h5>
         </div>
         <div className='stars'>
-          <h5><Stars key={this.props.house._id} rating={this.props.house.stars} /> {this.props.house.reviewCount}</h5>
+          <h5><Stars key={this.props.house._id} rating={this.props.house.stars} /> {this.props.house.reviewCount} </h5>
         </div>
-
-      </div>
+      </HouseContainer>
     )
   }
 }
