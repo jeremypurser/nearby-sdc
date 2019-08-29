500969c84e6dfef426aa9b4fd12601be0d007e06
tjlee527 <tjlee527@gmail.com>
2019-08-28 20:09:57 -0700

    correctly positioned heart button on image

--- client/components/Heart.jsx
+++ client/components/Heart.jsx
@@ -5,18 +5,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
 
-const HeartButton = styled.button`
+const HeartButton = styled.div`
   position: absolute;
-  top: 0;
+  top: 5%;
   right: 0;
   height: 20%;
-  width: 20%;
-  background-color: #555;
+  width: 15%;
+  background-color: transparent;
   color: white;
-  font-size: 16px;
   border: none;
-  cursor: pointer;
-  border-radius: 5px;
+  float: right;
 `
 
 class Heart extends React.Component {
@@ -36,7 +34,7 @@ class Heart extends React.Component {
 
   render() {
     return (
-    <HeartButton onClick={this.heartClick.bind(this)}>{this.state.clicked ? <FontAwesomeIcon icon={faHeart} size='2x'/> : <FontAwesomeIcon icon={farHeart} size='2x'/>}</HeartButton>
+    <HeartButton>{this.state.clicked ? <FontAwesomeIcon icon={faHeart} size='2x' onClick={this.heartClick.bind(this)}/> : <FontAwesomeIcon icon={farHeart} size='2x' onClick={this.heartClick.bind(this)}/> }</HeartButton>
     )
   }
 }
