94aec40e64c921e9735770c6751f61755813434b
Jeremy Purser <jeremypurser@gmail.com>
2019-09-11 15:04:51 -0400

    Remove whitespace

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Widget from './components/Widget.jsx';
 
-ReactDOM.render(< Widget />, document.getElementById('root'));
+ReactDOM.render(<Widget />, document.getElementById('root'));
