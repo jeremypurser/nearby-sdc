07396a0fbf991890d1bb75a626b06bbc0cafeb3b
Jeremy Purser <jeremypurser@gmail.com>
2019-09-11 14:29:55 -0400

    Disable no console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,6 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
-// module.exports = {
-//   extends: './node_modules/eslint-config-hackreactor/index.js'
-// };
-
-module.exports={
-  "extends": "airbnb-base",
-  "parser": "babel-eslint"
- };
-
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+      "no-console": 0
+    }
+};
