d62b5d43669ea9e86debd9aa82f2ac00cc4a78e8
Jeremy Purser <jeremypurser@gmail.com>
2019-09-11 14:30:15 -0400

    Update to airbnb stylem

--- server/index.js
+++ server/index.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const NearbyHouse = require('../database/NearbyHouse.js');
 
 const app = express();
 const port = 8081;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type'
-  })
+    'Access-Control-Allow-Headers': 'Content-Type',
+  });
   next();
 });
 
 app.get('/house/:houseId', (req, res) => {
-  NearbyHouse.find(({parentHouseId: req.params.houseId}))
+  NearbyHouse.find(({ parentHouseId: req.params.houseId }))
     .then((houses) => {
       res.send(houses);
     })
     .catch((error) => {
-      res.send(error)
-    })
+      res.send(error);
+    });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
 
 module.exports = app;
