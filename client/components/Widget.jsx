0d5902a5dfaa86c8555247d16a5cfdf35d089521
Jeremy Purser <jeremypurser@gmail.com>
2019-09-11 23:27:38 -0400

    Revert ternary style

--- client/components/Widget.jsx
+++ client/components/Widget.jsx
@@ -51,8 +51,8 @@ class Widget extends React.Component {
     return (
       <WidgetContainer id='morePlaces'>
         <h2>More places to stay</h2>
-        {this.state.view ?
-          <Carousel
+        {this.state.view
+          ? <Carousel
             key={this.state.currentHouse}
             changeCurrentHouse={this.changeCurrentHouse.bind(this)}
             nearbyHouseList={this.state.nearbyHouseList} />
