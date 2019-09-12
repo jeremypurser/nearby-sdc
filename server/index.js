176026c0844a5f7765223e3c385f453a73f3fdfc
Jeremy Purser <jeremypurser@gmail.com>
2019-09-11 20:38:37 -0400

    Implement CRUD API

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const NearbyHouse = require('../database/NearbyHouse.js');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 const app = express();
 const port = 8081;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use((req, res, next) => {
   res.set({
@@ -18,23 +20,68 @@ app.use((req, res, next) => {
 });
 
 // CREATE
-app.post();
+app.post('/house/:houseId', (req, res) => {
+  const {
+    nearbyNum, imgUrl, location, type, title, cost, stars, reviewCount, arrIndex
+  } = req.body;
+  console.log(nearbyNum, imgUrl, location);
+  NearbyHouse.create({
+    parentHouseId: req.params.houseId,
+    nearbyNum, imgUrl, location, type, title, cost, stars, reviewCount, arrIndex
+  })
+    .then(newHouse => {
+      console.log(`Save ${newHouse}`);
+      res.send(newHouse);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    });
+});
 
 // READ
 app.get('/house/:houseId', (req, res) => {
-  NearbyHouse.find(({ parentHouseId: req.params.houseId }))
-    .then((houses) => {
+  NearbyHouse.find({ parentHouseId: req.params.houseId })
+    .then(houses => {
       res.send(houses);
     })
-    .catch((error) => {
+    .catch(error => {
       res.send(error);
     });
 });
 
 // UPDATE
-app.patch();
+app.patch('/house/:houseId/:id', (req, res) => {
+  const updateObj = req.body;
+  const id = req.params.id;
+
+  NearbyHouse.findByIdAndUpdate({
+    _id: id
+  }, updateObj)
+    .then(updatedHouse => {
+      console.log(updatedHouse);
+      res.send(updatedHouse);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 // DELETE
+app.delete('/house/:houseId/:id', (req, res) => {
+  const updateObj = req.body;
+  const id = req.params.id;
+
+  NearbyHouse.findByIdAndDelete({
+    _id: id
+  }, updateObj)
+    .then(deleted => {
+      res.send();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
