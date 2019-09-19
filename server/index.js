4e205a0066f474fd04bca792ef6a379999eb4c6a
Jeremy Purser <jeremypurser@gmail.com>
2019-09-18 22:59:43 -0400

    Use controllers for REST API endpointss

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const NearbyHouse = require('../database/mongo/NearbyHouse.js');
-const ObjectId = require('mongoose').Types.ObjectId;
+const controller = require('./controllers.js');
 
 const app = express();
 const port = 8081;
 
-// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use((req, res, next) => {
@@ -19,69 +17,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// CREATE
-app.post('/house/:houseId', (req, res) => {
-  const {
-    nearbyNum, imgUrl, location, type, title, cost, stars, reviewCount, arrIndex
-  } = req.body;
-  console.log(nearbyNum, imgUrl, location);
-  NearbyHouse.create({
-    parentHouseId: req.params.houseId,
-    nearbyNum, imgUrl, location, type, title, cost, stars, reviewCount, arrIndex
-  })
-    .then(newHouse => {
-      console.log(`Save ${newHouse}`);
-      res.send(newHouse);
-    })
-    .catch(err => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-// READ
-app.get('/house/:houseId', (req, res) => {
-  NearbyHouse.find({ parentHouseId: req.params.houseId })
-    .then(houses => {
-      res.send(houses);
-    })
-    .catch(error => {
-      res.send(error);
-    });
-});
-
-// UPDATE
-app.patch('/house/:houseId/:id', (req, res) => {
-  const updateObj = req.body;
-  const id = req.params.id;
-
-  NearbyHouse.findByIdAndUpdate({
-    _id: id
-  }, updateObj)
-    .then(updatedHouse => {
-      console.log(updatedHouse);
-      res.send(updatedHouse);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-// DELETE
-app.delete('/house/:houseId/:id', (req, res) => {
-  const updateObj = req.body;
-  const id = req.params.id;
-
-  NearbyHouse.findByIdAndDelete({
-    _id: id
-  }, updateObj)
-    .then(deleted => {
-      res.send();
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+app.post('/house/:houseId', controller.createRental);
+app.get('/house/:houseId', controller.findNearbyRentals);
+app.put('/house/:houseId/:id', controller.updateRental);
+app.delete('/house/:houseId/:id', controller.deleteRental);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
