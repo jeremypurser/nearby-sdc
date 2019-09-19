3bb1ac9836c46407455b3a2603a515cde43ff393
Jeremy Purser <jeremypurser@gmail.com>
2019-09-19 19:46:48 -0400

    Add PATCH to CORS

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '/../public')));
 app.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE',
     'Access-Control-Allow-Headers': 'Content-Type',
   });
   next();
